ef0e9319d82a687759b0ffb2f74fdc98172214b1
Eric Garner <ericgarner81@gmail.com>
2023-10-30 14:48:30 -0500

    completed exercise

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace FactoryPattern
+﻿using System.Buffers.Text;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace FactoryPattern
+//    Create an Interface named IVehicle that has a stubbed out public void Drive method.
+//Create 2 new public classes that will conform to IVehicle.
+//Example) Car, Motorcycle, BigRig
+//Note: These classes must conform to IVehicle and implement the Drive() method -
+//which will just Console.WriteLine(“Building a new Car!”)
+
+//Now we will make our static VehicleFactory class.
+//It will contain a static method GetVehicle(), that will return an IVehicle based on the
+//amount of tires it’s given as a parameter
+//Call this functionality in the Main method.
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool trueOrFalse = true;
+            while (trueOrFalse)
+            {
+                Console.WriteLine("How many tires do you want on your vehicle?");
+
+                var userValue = Console.ReadLine();
+                int inputValue;
+
+                bool success = int.TryParse(userValue, out inputValue);
+
+                if (success)
+                {
+
+                    Ivehicle vehicle = VehicleFactory.GetVehicle(inputValue);
+
+                    vehicle.Drive();
+
+                    trueOrFalse = false;
+
+                }
+
+                else 
+                { 
+                    Console.WriteLine("Please enter a valid integer for tires.");
+                    Console.WriteLine();
+                }
+            }
+
+            
+
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    { 
+        public static Ivehicle GetVehicle(int numTires)
+        {
+            switch (numTires)
+            {
+                case 4:
+
+                    return new Car();
+
+                case 2:
+                case 3:
+                    return new Motorcycle();
+
+                default:
+                    return new Car();
+
+
+
+            }
+        }
+    }
+}
